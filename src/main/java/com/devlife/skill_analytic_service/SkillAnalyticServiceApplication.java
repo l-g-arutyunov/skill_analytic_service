a6b232e0e407b055fd398e0db55c01c438ffbcab
larutyunov <larutyunov@diasoft.ru>
2022-08-16 00:21:09 +0300

    Initial commit

---
+++ src/main/java/com/devlife/skill_analytic_service/SkillAnalyticServiceApplication.java
@@ -0,0 +1,13 @@
+package com.devlife.skill_analytic_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillAnalyticServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillAnalyticServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devlife/skill_analytic_service/SkillAnalyticServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlife.skill_analytic_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillAnalyticServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
